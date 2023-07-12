cc1e2fd95bce2825e1b043cc99f8c722dd780c42
Rohan <nikeshshrestha405@gmail.com>
2023-07-12 23:10:24 +0545

    inital commit

---
+++ darkmode.js
@@ -0,0 +1,12 @@
+// var darkmoode = document.getElementById("darkmode");
+// darkmode.onclick = function(){
+//   document.body.classList.toggle("dark-mode")
+// }
+// var bluemodeode = document.getElementById("bluemode");
+// bluemodeode.onclick = function(){
+//   document.body.classList.toggle("bluemode")
+// }
+var darkmode= document.getElementById("check");
+darkmode.onclick = function(){
+    document.body.classList.toggle("dark-mode")
+    }
